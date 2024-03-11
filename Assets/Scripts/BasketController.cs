2a466db472e65e34a6164b9dd88b1fca452f2300
mjpurin <mjpurin@gmail.com>
2024-03-11 14:00:08 +0900

    SE追加

--- Assets/Scripts/BasketController.cs
+++ Assets/Scripts/BasketController.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class BasketController : MonoBehaviour
 {
+    public AudioClip appleSE;
+    public AudioClip bombSE;
+    AudioSource aud;
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate=60;
-        
+        this.aud = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,5 +27,15 @@ public class BasketController : MonoBehaviour
             }
         }
         
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (other.CompareTag("Apple")){
+            this.aud.PlayOneShot(this.appleSE);
+
+        }else{
+            this.aud.PlayOneShot(this.bombSE);
+        }
+        Destroy(other.gameObject);
     }
 }
